c7a52460fe1a53250be582fa6aaa61c6d3c9b634
Javier Ramirez Cospin <ram18099@uvg.edu.gt>
2023-09-03 15:04:35 -0600

    - Added displayMenu function in Main.java to display Menu to user - Added getTopology function in Main.java to define & get Network Topology

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,28 @@
 package src;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hola mundo");
+        // GET TOPOLOGY
+        ArrayList<ArrayList<String>> topology = getTopology();
+        String user_name = "";
+
+    }
+
+    public static void displayMenu() {
+        System.out.println("=== CLIENT MENU ===\n" +
+                "1. Enter \n");
+    }
+
+    public static ArrayList<ArrayList<String>> getTopology() {
+        // DEFINE NETWORK TOPOLOGY (NON DIRECTIONAL)
+        ArrayList<ArrayList<String>> topology = new ArrayList<>();
+        // (list[0] -> list[1] -> list[2]) | (list[2] -> list[1] -> list[0])
+        topology.add(new ArrayList<>(Arrays.asList("A", "1", "B")));
+        topology.add(new ArrayList<>(Arrays.asList("A", "3", "C")));
+        topology.add(new ArrayList<>(Arrays.asList("B", "2", "C")));
+        return topology;
     }
 }
